01026b2543f689049b30cde7b62c7e70e38e6faf
crow0525crow <crow0525crow@gmail.com>
2020-08-31 11:07:40 +0900

    ミニマップ作成

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Follow : MonoBehaviour
+{
+
+    void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+
+}
+
