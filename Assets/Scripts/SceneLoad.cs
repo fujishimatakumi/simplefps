cbf22dff7a3dc6424fe6b216440b7ea880ace642
hujishima takumi <hujishima.t425@gmail.com>
2020-10-13 19:15:26 +0900

    シーンを読み込むためのスクリプトSceneLoadを作成しました

---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    [SerializeField] string m_loadSceneName;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(m_loadSceneName);
+    }
+
+    public void LoadSceneDelay(float delay)
+    {
+        StartCoroutine(Delay(delay, m_loadSceneName));
+    }
+
+    private IEnumerator Delay(float delay,string sceneName)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
